b0a760f83a494123a8e156684920cfa44fd7eb16
Frank Zheng <zmyfrank@qq.com>
2022-07-18 15:49:39 +0800

    feat: init

---
+++ src/pages/test/component/testComponents.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="test-wrapper"> {{ testMsg }} </div>
+</template>
+<script setup lang="ts">
+  const testMsg = ref<string>('test');
+</script>
+<style lang="less" scoped>
+  .test-wrapper {
+    height: 100%;
+  }
+</style>
