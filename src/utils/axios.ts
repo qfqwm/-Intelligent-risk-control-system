4da36e4e0366833128082e5e5e3a041ec6cf08da
王方璐 <2207387983@qq.com>
2022-08-12 07:15:52 +0000

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,10 +2,10 @@
 import axios from 'axios';
 const api = axios.create({
   //服务器
-  // baseURL: 'http://120.26.203.148:8848',
+   baseURL: 'http://120.26.203.148:8848',
 
   //ztc测试环境
-  baseURL: 'http://10.8.21.137:8888',
+  //baseURL: 'http://10.8.21.137:8888',
 
   // baseURL:"http://172.20.10.10:8080",
   // headers:{"Content-Type":"application/json;charset=utf-8"}
